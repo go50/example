fce549478270e0002f7bcac0fbae73ad9eec95d3
go50 <go50@jp.fujitsu.com>
2014-03-23 16:41:44 +0900

    emboss added

---
+++ biodevops/site-cookbooks/emboss/README.md
@@ -0,0 +1,13 @@
+# emboss cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ biodevops/site-cookbooks/emboss/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: emboss
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+package 'emboss' do
+  action :install
+end
